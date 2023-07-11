0cc76d707d2a7fae5db585c2943c65ca28e0dd42
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2023-07-11 10:29:38 -0300

    feat: pages (controller)

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :assemblies_parts
-  resources :suppliers
-  resources :books
-  resources :authors
   scope "(:locale)", locale: /pt-BR|en/ do
-
+    root 'pages#home'
+    resources :assemblies_parts
+    resources :suppliers
+    resources :books
+    resources :authors
   end
 end
